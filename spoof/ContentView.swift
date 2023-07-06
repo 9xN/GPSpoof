331bb19e8d40d69b8a5b374858521f86f10bde2c
social reject <94141577+9xN@users.noreply.github.com>
2023-07-06 15:31:46 -0600

    initpt3

---
+++ spoof/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  spoof
+//
+//  Created by Alex on 2023-07-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ spoof/spoofApp.swift
@@ -0,0 +1,17 @@
+//
+//  spoofApp.swift
+//  spoof
+//
+//  Created by Alex on 2023-07-06.
+//
+
+import SwiftUI
+
+@main
+struct spoofApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
